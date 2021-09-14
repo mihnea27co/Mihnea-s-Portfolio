606d0a177993a5c9d078825f2c140cdf168031a2
mihneacojocaru <mihnea.co@gmail.com>
2021-09-14 18:52:29 +0200

    first title

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-const menuBtn = document.querySelector('.menu-button');
+const menuBtn = document.querySelector(".menu-button");
 console.log(menuBtn);
 let menuOpen = false;
 
-menuBtn.addEventListener('click', () => {
-    menuBtn.classList.toggle("open");
-    let navList = document.querySelector('#navList');
-    let showHide = document.querySelector('.showHide');
-    if(showHide){
-        navList.classList.remove('showHide');
-    } else{
-        navList.classList.add('showHide');
-    }
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("open");
+  let navList = document.querySelector("#navList");
+  let showHide = document.querySelector(".showHide");
+  if (showHide) {
+    navList.classList.remove("showHide");
+    let bod = document.getElementsByTagName("BODY");
+    bod.style = "";
+  } else {
+    navList.classList.add("showHide");
+    let bod = document.getElementsByTagName("BODY");
+    bod.style = "height: 100vh";
+  }
 });
